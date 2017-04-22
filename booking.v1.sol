ca2ccc4b491719bc7ae4da0fe29f4c641e10f2ba
Eric Maino <ericmai@microsoft.com>
2017-04-21 17:17:14 -0700

    WIP

---
+++ booking.v1.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.4.8;
+
+contract Booking 
+{
+    struct BookingData
+    {
+        address principal;
+        uint price;
+    }
+
+    BookingData party1;
+    BookingData party2;
+    string public referenceId;
+    string public activeState;
+
+    function Booking(string reference, address externalParty, uint price) 
+    {
+        referenceId = reference;
+        updateBooking(reference, externalParty, price);
+    }
+
+    function updateBooking(string reference, address externalParty, uint price) {
+        address me = tx.origin;
+        bool party1 = me < externalParty;
+        BookingData currentParty;
+        
+        if (party1)
+        {
+            currentParty = party1;
+        }
+        else
+        {
+            currentParty = party2;
+        }
+
+        if (currentParty.principal = 0x0)
+        {
+            currentParty.party1 = me;
+        }
+
+        currentParty.price = price;
+        updateState();
+    }
+
+    function updateState() private
+    {
+        if (party1.principal == 0x0)
+        {
+            activeState = "Incomplete";
+        }
+        else if (party2.principal == 0x0)
+        {
+            activeState = "Incomplete";
+        }
+        else if (party1.price == party2.price)
+        {
+            activeState = "Agreed";
+        }
+        else 
+        {
+            activeState = "Disagreed";
+        }
+    }
+}
